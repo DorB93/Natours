888fe74d469078d50644a903ea249f759563828e
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-05 16:51:59 +0200

    Implementing Rate Limiting

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const errorMiddleware = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
@@ -8,11 +10,19 @@ const userRouter = require(`${__dirname}/routes/userRoutes`);
 
 const app = express();
 
-// Middleware
+// Global Middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:
+    'Too many requests from this IP, please try again in an hour',
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
