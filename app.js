44f6b3f7f65b2a75aeaa7a7a225fb7b0c5c7f8b1
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-05 17:19:28 +0200

    Add Data Sanitization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const errorMiddleware = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -11,10 +14,15 @@ const userRouter = require(`${__dirname}/routes/userRoutes`);
 const app = express();
 
 // Global Middleware
+// Set security HTTP headers
+app.use(helmet());
+
+// Development loggin
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from the same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,9 +31,20 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body parser, readind data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
+
+// Servings statics files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
