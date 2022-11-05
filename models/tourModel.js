320ac15d9e37321e24c3fdbbb8d91cafbd285f15
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-05 21:56:05 +0200

    Modelling Tour Guides via Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
+const User = require('./userModel');
+
 // Creating tour schema
 const tourSchema = new mongoose.Schema(
   {
@@ -111,6 +113,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -125,6 +128,14 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save()and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(
+    async (id) => await User.findById(id),
+  );
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
