9443b7a7680010fb8c2eaf79365c758900d58e89
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-05 15:48:05 +0200

    Add deleteUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const User = require('./../models/userModel');
 
 function checkUserID(req, res, next, val) {
@@ -5,6 +6,15 @@ function checkUserID(req, res, next, val) {
   next();
 }
 
+function filterObj(obj, ...allowedFields) {
+  let filteredObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      filteredObj[el] = obj[el];
+    }
+  });
+  return filteredObj;
+}
 async function getAllUsers(req, res, next) {
   try {
     const users = await User.find();
@@ -14,6 +24,52 @@ async function getAllUsers(req, res, next) {
       data: {
         users,
       },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateMe(req, res, next) {
+  try {
+    // 1) Create error if user POSTs Password data
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(
+        new AppError(
+          'This route is not for password updates!!. Please use /updateMyPassword route!',
+          400,
+        ),
+      );
+    }
+    // 2) Filtered out unwanted fields names that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    // 3) update user doc
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user._id,
+      filteredBody,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteMe(req, res, next) {
+  try {
+    await User.findByIdAndUpdate(req.user._id, { active: false });
+    res.status(204).json({
+      status: 'success',
+      data: null,
     });
   } catch (err) {
     next(err);
@@ -81,4 +137,6 @@ module.exports = {
   updateUser,
   deleteUser,
   checkUserID,
+  updateMe,
+  deleteMe,
 };
