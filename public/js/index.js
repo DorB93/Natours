5eb0e18663f19b4eae45e8253823287fe687effd
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-27 17:08:43 +0200

    Implement Update user password

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,16 @@ import 'core-js/stable';
 import { displayMap } from './leaflet';
 import { login } from './login';
 import { logout } from './logout';
-import { updateData } from './updatesettings';
+import { updateSettings } from './updatesettings';
 
 // Dom Elements
 const mapLeaflet = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
-const updateUserForm = document.querySelector('.form-user-data');
+const updateDataUserForm = document.querySelector('.form-user-data');
+const updatePasswordUserForm = document.querySelector(
+  '.form-user-settings',
+);
 // Values
 
 // Delegation
@@ -29,12 +32,34 @@ if (loginForm) {
 if (logOutBtn) {
   logOutBtn.addEventListener('click', logout);
 }
-if (updateUserForm) {
-  updateUserForm.addEventListener('submit', (e) => {
+if (updateDataUserForm) {
+  updateDataUserForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const name = document.getElementById('name').value;
     console.log(email);
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+if (updatePasswordUserForm) {
+  updatePasswordUserForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const btn = document.querySelector('.btn-user-settings');
+    btn.value = 'Updating....';
+    const password = document.getElementById(
+      'password-current',
+    ).value;
+    const newPassword = document.getElementById('password').value;
+    const newPasswordConfirm = document.getElementById(
+      'password-confirm',
+    ).value;
+    await updateSettings(
+      { password, newPassword, newPasswordConfirm },
+      'password',
+    );
+    btn.value = 'Complete!';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,25 +1,29 @@
 import { showAlert } from './alerts';
 
-export async function updateData(name, email) {
+/**
+ *
+ * @param {{email,name}|| {password,newPassword,newPasswordConfirm}} data
+ * @param {'password' || 'data'} type
+ */
+export async function updateSettings(data, type) {
   try {
-    console.log({ name }, { email });
-    const req = await fetch(
-      'http://127.0.0.1:3000/api/v1/users/updateMe',
-      {
-        method: 'PATCH',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          name,
-        }),
+    const url =
+      type === 'data'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMe'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMyPassword';
+    const req = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify({
+        ...data,
+      }),
+    });
     const res = await req.json();
     if (res.status === 'success') {
-      showAlert('success', 'Your update completed');
+      showAlert('success', `'Your ${type} update completed`);
       window.setTimeout(() => {
         location.reload(true);
       }, 1000);
