3f52468033be792d6e796b060a0c08b80191dc8d
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-01 13:25:14 +0200

    remove test middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -183,11 +183,6 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} MS`);
-  next();
-});
-
 // Aggregation Middleware
 // tourSchema.pre('aggregate', function (next) {
 //   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
