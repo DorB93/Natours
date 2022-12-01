3636713166e3674d3531a33768a8d3677694d075
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-01 23:28:57 +0200

    test booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -48,11 +48,11 @@ async function createBookingCheckout(session) {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))
     ._id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 }
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
@@ -64,10 +64,11 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (error) {
     return res.status(400).send(`Webhook error: ${error.message}`);
   }
+  let booking = {};
   if (event.type === 'checkout.session.completed') {
-    createBookingCheckout(event.data.object);
+    booking = await createBookingCheckout(event.data.object);
   }
-  res.status(200).json({ received: true });
+  res.status(200).json({ received: true, booking });
 };
 exports.getAllBookings = factory.getAll(Booking);
 exports.getBooking = factory.getOne(Booking);
